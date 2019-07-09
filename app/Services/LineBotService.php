a5b467848e42c80ae8bc8d6d09e1d8b03c6124f9
r567tw <r567tw@gmail.com>
2019-07-10 06:28:10 +0800

    image message

--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -2,6 +2,10 @@
 namespace App\Services;
 
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
 
 class LineBotService
 {
@@ -26,6 +30,33 @@ class LineBotService
     {
     }
 
+
+    public function buildImagesMessageBuilder(array $data, string $notificationText = 'Hello 這是今日的金句!')
+    {
+        $imageCarouselColumnTemplateBuilders = array_map(function ($d) {
+            return $this->buildImageCarouselColumnTemplateBuilder(
+                $d['imagePath'],
+                $d['directUri'],
+                $d['label']
+            );
+        }, $data);
+
+        $tempChunk = array_chunk($imageCarouselColumnTemplateBuilders, 5);
+
+        return array_map(function ($data) use ($notificationText) {
+            return new TemplateMessageBuilder(
+                $notificationText,
+                new ImageCarouselTemplateBuilder($data)
+            );
+        }, $tempChunk);
+    }
+
+    private function buildImageCarouselColumnTemplateBuilder(string $imagePath, string $directUri, string $label){
+        return new ImageCarouselColumnTemplateBuilder(
+            $imagePath,
+            new UriTemplateActionBuilder($label, $directUri)
+        );
+    }
 
     public function pushMessage($content)
     {
--- tests/Unit/Services/LineBotServiceTest.php
+++ tests/Unit/Services/LineBotServiceTest.php
@@ -21,4 +21,27 @@ class LineBotServiceTest extends TestCase
 
         $this->assertEquals(200, $response->getHTTPStatus());
     }
+
+    public function testBuildImagesMessageBuilder()
+    {
+        $lineService = new LineBotService(env('LINE_USER_ID'));
+
+        $data = [
+                    [
+                        'imagePath' => 'https://i.imgur.com/BlBH2HE.jpg',
+                        'directUri' => 'https://github.com/Tai-ch0802/php-crawler-chat-bot',
+                        'label' => '自己玩的linebot',
+                    ],
+                    [
+                        'imagePath' => 'https://i.imgur.com/XJkiup5.jpg',
+                        'directUri' => 'https://zh.wikipedia.org/wiki/%E7%8C%AB',
+                        'label' => '喵星人',
+                    ]
+        ];
+        $targets = $lineService->buildImagesMessageBuilder($data, '今日金句');
+        foreach ($targets as $target) {
+            $response =  $lineService->pushMessage($target);
+            $this->assertEquals(200, $response->getHTTPStatus());
+        }
+    }
 }
