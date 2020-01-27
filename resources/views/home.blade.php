31f83801e9c9274db117db8610a16f0974279fc3
r567tw <r567tw@gmail.com>
2020-01-28 07:31:29 +0800

    add days time informations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,25 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
+                    @auth
+                    Hi, {{ auth()->user()->name }}. You are logged in!
+                    @endauth
+                    <br />
+                    <br />
+                    <p class="alert alert-success">
+                        現在是 {{ $today->format('Y-m-d') }}，<br />
+                        今年的第{{ $today->weekOfYear }}週<br />
+                        今年的第{{ $today->dayOfYear }}天<br />
+                    </p>
 
-                    You are logged in!
+                    <p class="alert alert-info">
+                        今年還剩下{{ $today->weeksInYear-$today->weekOfYear }}週<br />
+                        今年還剩下{{ $today->daysInYear-$today->dayOfYear }}天<br />
+                    </p>
                 </div>
             </div>
         </div>
