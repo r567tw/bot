b64b65918a0b8df32ff43cc17a62745a66ccc53e
r567tw <r567tw@gmail.com>
2020-10-22 02:45:35 +0800

    改寫成laravel 7 HTTP Client 寫法

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -2,42 +2,20 @@
 
 namespace App\Http\Controllers;
 
-// use Illuminate\Support\Facades\Http;
-use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 
 class LineController extends Controller
 {
-    private $client;
-
-    public function __construct(Client $client)
-    {
-        $this->client = $client;
-    }
-
     public function notify()
     {
         $token = env('LINE_NOTIFY_TOKEN');
 
-        $options = [
-            'headers' => [
-                'Authorization' => "Bearer {$token}",
-            ],
-            'form_params' => [
-                'message' => '測試通知成功！',
-            ],
-        ];
+        $response = Http::withToken($token)->withHeaders([
+            'Content-Type' => 'application/x-www-form-urlencoded',
+        ])->asForm()->post('https://notify-api.line.me/api/notify', [
+            'message' => request()->get('message', '測試訊息'),
+        ]);
 
-        $url = 'https://notify-api.line.me/api/notify';
-
-        $response = $this->client->post($url, $options);
-
-        // $response = Http::withHeaders([
-        //     'Content-Type' => 'multipart/form-data',
-        //     'Authorization' => "Bearer {$token}",
-        // ])->post('https://notify-api.line.me/api/notify', [
-        //     'message' => '測試通知！',
-        // ]);
-
-        return response()->json(['message' => 'success']);
+        return response()->json(['data' => $response->body()]);
     }
 }
